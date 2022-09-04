7d62a29d4b27768e01082465268e21469833f096
podhmo <ababjam61+github@gmail.com>
2022-09-05 02:50:26 +0900

    chore: adjust internal/x (broken)

--- internal/x/method.go
+++ internal/x/method.go
@@ -3,9 +3,16 @@ package x
 func (w *W) Method(s S) {
 	G0()
 	defer log()()
+	W0{}.M()
 	G0()
 }
 
 func (w W) String() string { return "W" }
 
 type W struct{}
+
+type W0 struct{}
+
+func (w W0) M() {
+	G0()
+}

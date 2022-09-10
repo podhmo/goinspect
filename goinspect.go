072cf2825a7903d8bd3d0cf1444174cc1aefff34
podhmo <ababjam61+github@gmail.com>
2022-09-10 15:08:09 +0900

    refactor: rename method

--- goinspect.go
+++ goinspect.go
@@ -19,7 +19,7 @@ type Config struct {
 	OtherPackages     []string
 }
 
-func (c *Config) Need(name string) bool {
+func (c *Config) NeedName(name string) bool {
 	return c.IncludeUnexported || token.IsExported(name)
 }
 
@@ -65,13 +65,13 @@ func Dump(c *Config, g *Graph, nodes []*Node) error {
 		prefix := strings.Join(parts[:len(parts)-1], "/") + "/"
 		if len(path) == 1 {
 			node := path[0]
-			if len(node.From) == 0 && c.Need(node.Name) {
+			if len(node.From) == 0 && c.NeedName(node.Name) {
 				name := strings.ReplaceAll(path[len(path)-1].Value.Object.String(), prefix, "")
 				fmt.Printf("\n%s%s\n", strings.Repeat("  ", len(path)), name)
 			}
 		} else {
 			node := path[len(path)-1]
-			if c.Need(node.Name) {
+			if c.NeedName(node.Name) {
 				name := strings.ReplaceAll(node.Value.Object.String(), prefix, "")
 				fmt.Printf("%s%s\n", strings.Repeat("  ", len(path)), name)
 			}
